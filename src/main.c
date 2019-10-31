31889c6c15624b084397626d474752ac1a4b7c40
brenfeliped <brenofeliped15@live.com>
2019-10-31 15:26:05 -0300

    create threads

--- src/main.c
+++ src/main.c
@@ -6,18 +6,28 @@
 int matrix[4][4]; 
 pthread_mutex_t  acesso[4][4];
 
-void * trail(void * id, void * numTrail){
+void * trail(void * id){
     
 }
 void * car(void * id){
 
 }
 int main(){
+    int * id;
+    pthread_t trails[4];
+    pthread_t player;
     for(int i=0;i<4;i++){
-        for(int j=-0;j<4;j++){
+        for(int j=0;j<4;j++){
             matrix[i][j]=0;
         }
     }
-    
+    for(int i=0;i<4;i++){
+        id = (int *)malloc(sizeof(int));
+        *id =i;
+        pthread_create(&trails[i],NULL,trail,(void *) id);
+    }
+    id = (int *)malloc(sizeof(int));
+    *id =99;
+    phread_create(&player,NULL,car,(void *)id);
     return 0;
 }
