03d2a3ac631ce199429d2e7b82f6f9fde521eaa4
brenfeliped <brenofeliped15@live.com>
2019-11-09 19:05:14 -0200

    quase la

--- src/main.c
+++ src/main.c
@@ -3,10 +3,46 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
-#ifdef _WIN32
-#define CLEAR "cls"
+#ifndef _WIN32
+    #include <termios.h>
+    #include <fcntl.h>
+    int kbhit(){
+        struct termios oldt, newt;
+        int ch, oldf;
+        tcgetattr(STDIN_FILENO,&oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+        ch = getchar();    
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        fcntl(STDIN_FILENO, F_SETFL, oldf);
+    if( ch != EOF){
+        ungetc(ch,stdin);
+        return 1;
+    }
+    return 0;
+    }
+    int getch(void) {
+        int ch;
+        struct termios oldt;
+        struct termios newt;
+        tcgetattr(STDIN_FILENO,&oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        return ch;
+    }
 #else
-#define CLEAR "clear"
+    #include <conio.h>
+#endif
+#ifdef _WIN32
+    #define CLEAR "cls"
+#else
+    #define CLEAR "clear"
 #endif
 #define RED  "\033[0;31m"
 #define GREEN "\033[0;32m"
@@ -21,7 +57,9 @@ void * car(void *);
 
 int lifes = 3;
 int points=0;
-
+int playerCol = 1;
+int playerRow = 8;
+char tecla;
 char matrix[9][4];
 int CountThreads=0;
 pthread_mutex_t  acesso[9][4];
@@ -89,57 +127,58 @@ void screen(){
 }
 // end Interface funtions
 void * trail(void * id){
-    int j=0;
+    int i=0;
     int * numtrail = (int *) id;
     int idTrail = * numtrail;
     //printf("IdTrail = %d\n",idTrail);
-    while(1){
-        pthread_mutex_lock(&acesso[j][idTrail]);
-        matrix[j][idTrail] = idTrail;
-        if(j==0){
+    while(tecla != 'q'){ // condicao temporaria
+        pthread_mutex_lock(&acesso[i][idTrail]);
+        matrix[i][idTrail] = idTrail;
+        if(i==0){
             switch (idTrail){
             case 0:
-                matrix[j][idTrail] = '!';
+                if(matrix[8][idTrail]=='!')matrix[8][idTrail]='0';
+                matrix[i][idTrail] = '!';
                 break;
             case 1:
-                matrix[j][idTrail] = 'P';
+                if(matrix[8][idTrail]=='P')matrix[8][idTrail]='0';
+                matrix[i][idTrail] = 'P';
                 break;
             case 2:
-                matrix[j][idTrail] = '&';
+                if(matrix[8][idTrail]=='&')matrix[8][idTrail]='0';
+                matrix[i][idTrail] = '&';
                 break;
             default:
-                matrix[j][idTrail] = '#';
+                if(matrix[8][idTrail]=='U')matrix[8][idTrail]='0';
+                matrix[i][idTrail] = 'U';
                 break;
             }
         }else{
-            matrix[j-1][idTrail] = '0';
+            matrix[i-1][idTrail] = '0';
             switch (idTrail){
             case 0:
-                matrix[j][idTrail] = '!';
+                matrix[i][idTrail] = '!';
                 break;
             case 1:
-                matrix[j][idTrail] = 'P';
+                matrix[i][idTrail] = 'P';
                 break;
             case 2:
-                matrix[j][idTrail] = '&';
+                matrix[i][idTrail] = '&';
                 break;
             default:
-                matrix[j][idTrail] = '#';
+                matrix[i][idTrail] = 'U';
                 break;
             }
         }
-        pthread_mutex_unlock(&acesso[j][idTrail]);
-        if(j==8)matrix[j][idTrail] = '0';
-        j++;
-        if(j==9) j=0;
-        sleep(idTrail+1);
+        pthread_mutex_unlock(&acesso[i][idTrail]);
+        i++;
+        if(i==9) i=0;
+        sleep(idTrail*1+1);
     }
     //sleep(1);
     pthread_mutex_lock(&somaThreads);
     CountThreads+=1;
     pthread_mutex_unlock(&somaThreads);
-    //printf("Incrementei o count = %d  e idTrail=%d\n",CountThreads,idTrail);
-    //sleep(1);
     pthread_exit(NULL);
     //return NULL;
 }
@@ -152,21 +191,79 @@ void  printMatrix(){
     }
 }
 void * car(void * id){
-    char caracter;
-    while(1){
-        scanf("%c",&caracter);
-        printf("Estou na car\n");
+    //char tecla;
+    while(tecla!='q'){
+        if(kbhit()==1){ // se uma tecla for digitada
+            tecla = (char)getch();
+            switch (tecla){
+            case 'd':
+                if(playerCol!=3){
+                    playerCol+=1;
+                    if(pthread_mutex_trylock(&acesso[playerRow][playerCol])){ // posicao a  direita ocupada
+                        playerCol-=1; // continua na mesma posicao
+                    }else{
+                        matrix[playerRow][playerCol-1] = '0';
+                        matrix[playerRow][playerCol] = '@';
+                        pthread_mutex_unlock(&acesso[playerRow][playerCol]);
+                    }
+                }
+                break;
+            case 'a':
+                if(playerCol!=0){
+                    playerCol-=1;
+                    if(pthread_mutex_trylock(&acesso[playerRow][playerCol])){// posicao a  esquerda ocupada
+                        playerCol+=1;
+                    }else{
+                        matrix[playerRow][playerCol+1] = '0';
+                        matrix[playerRow][playerCol] = '@';
+                        pthread_mutex_unlock(&acesso[playerRow][playerCol]);
+                    }
+                }
+                break;
+            case 's':
+                if(playerRow!=8){
+                    playerRow+=1;
+                    if(pthread_mutex_trylock(&acesso[playerRow][playerCol])){// posicao abaixo  ocupada
+                        playerRow-=1;
+                    }else{
+                        matrix[playerRow-1][playerCol] = '0';
+                        matrix[playerRow][playerCol] = '@';
+                        pthread_mutex_unlock(&acesso[playerRow][playerCol]);
+                    }
+                }
+                break;
+            case 'w':
+                if(playerRow!=0){
+                    playerRow=playerRow-1;
+                    if(pthread_mutex_trylock(&acesso[playerRow][playerCol])){//posicao acima ocupada(morte)
+                        // implementa a morte e diminuicao de life
+                        tecla='q'; // temporario
+                    }else{
+                        matrix[playerRow+1][playerCol] = '0';
+                        matrix[playerRow][playerCol] = '@';
+                        pthread_mutex_unlock(&acesso[playerRow][playerCol]);
+                    }
+                }
+                break;
+            default:
+                break;
+            }
+        }
     }
-    return NULL;
+    pthread_exit(NULL);
+}
+void loopGame(){
+    
 }
 int main(){
     int * id;
     pthread_t trails[4];
     pthread_t player;
     //pthread_t screen;
-      for(int i=0;i<9;i++){ // iniciando a matriz
+      for(int i=0;i<9;i++){ // iniciando a matriz logica
         for(int j=0;j<4;j++){
-            matrix[i][j]='0';
+            if(i==8 && j==1)matrix[i][j]='@'; // posicao incial player
+            else matrix[i][j]='0';
         }
     }
     for(int i=0;i<9;i++){ // iniciando os mutex
@@ -182,10 +279,9 @@ int main(){
         pthread_create(&trails[i],NULL,trail,(void *) id);
     }
     pthread_create(&player,NULL,car,NULL);
-    sleep(1);
     //pthread_create(&screen,NULL,&printMatrix,NULL);
     while(CountThreads<3){
-        sleep(2);
+        usleep(10500);
         screen();
         printf("\n");
     } // loop de espera
@@ -194,12 +290,10 @@ int main(){
             pthread_mutex_destroy(&acesso[i][j]);
         }
     }
-    //printf("Detruir os mutex\n");
     pthread_mutex_destroy(&somaThreads);
     //pthread_join((pthread_t)&player,NULL);
     //printf("Cheguei no printMatrix\n");
-    sleep(1);
-    printMatrix();
-    //pthread_join(&screen,NULL);
+    //sleep(1);
+    //printMatrix();
     return 0;
 }
